11d8b297b07e0fcc9c7bc7852d6b0b73169f0b46
Caio Sella <sellacaio4@gmail.com>
2023-08-26 16:53:17 -0300

    First commit | basic project

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,15 @@
+import {Text} from 'react-native';
+
+import { Container, Title } from './styles';
+
+const Groups = () => {
+  return (
+    <Container>
+      <Title >
+        Caio
+      </Title>
+    </Container>
+  );
+}
+
+export default Groups;
